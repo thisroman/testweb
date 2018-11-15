e530e33aa6586eb4eb27dccb0215eb74c8cb78c8
rn <rn@intravision.dk>
2018-11-15 11:27:59 +0200

    Test commit.

--- pwSter/src/main/java/coz/zell/servl/CozzServlet.java
+++ pwSter/src/main/java/coz/zell/servl/CozzServlet.java
@@ -1,6 +1,5 @@
 package coz.zell.servl;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,15 +7,15 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class CozzServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter writer = response.getWriter();
         writer.write("<h1>--  TRUE 22 --</h1>");
         writer.flush();
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter writer = response.getWriter();
-        writer.write("<h1>--  TRUE --</h1>");
+        writer.write("<h1>--  TRUE 11 --</h1>");
         writer.flush();
     }
 }
